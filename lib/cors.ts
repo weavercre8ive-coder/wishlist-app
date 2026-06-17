d82eb80b49123a137dd5c1f971659c030362e0fd
Md Rakibul Hasan <mdroquibulhasan@gmail.com>
2026-06-17 14:47:53 +0600

    Fix CORS for weavercre8ive.com

--- lib/cors.ts
+++ lib/cors.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 
 export function corsHeaders() {
   return {
-    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Origin': 'https://weavercre8ive.com',
     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
     'Access-Control-Allow-Headers': 'Content-Type, Authorization',
   };
